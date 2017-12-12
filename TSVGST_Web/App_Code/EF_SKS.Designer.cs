6bdbb669e0214f7526b72eb38bb0f1416e97ca27
anilkumarshah <anil.shah007@gmail.com>
2017-12-12 22:48:04 +0530

    Done the changes at home on 12Dec2017

--- TSVGST_Web/App_Code/EF_SKS.Designer.cs
+++ TSVGST_Web/App_Code/EF_SKS.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\TranServAPIUtility\TSVGST_Web\App_Code\EF_SKS.edmx'. 
+﻿// T4 code generation is enabled for model 'H:\ConnectingSolution\ConnectingSolution\TSVGST_Web\App_Code\EF_SKS.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
